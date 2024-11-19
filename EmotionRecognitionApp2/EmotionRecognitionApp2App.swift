b9caaeb1d35b13b99dc2478f91d848fab0cc5255
HansolNam <namangsoll@gmail.com>
2024-11-19 21:56:05 +0900

    Initial Commit

---
+++ EmotionRecognitionApp2/EmotionRecognitionApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionRecognitionApp2App.swift
+//  EmotionRecognitionApp2
+//
+//  Created by hansol on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct EmotionRecognitionApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmotionRecognitionApp2Tests/EmotionRecognitionApp2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionRecognitionApp2Tests.swift
+//  EmotionRecognitionApp2Tests
+//
+//  Created by hansol on 11/19/24.
+//
+
+import Testing
+@testable import EmotionRecognitionApp2
+
+struct EmotionRecognitionApp2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
